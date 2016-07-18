440642832ca2e506b6c1cae31c47433648cec65b
Anu Rajaram (nee Puthur-V) <anurajaram23@gmail.com>
2016-07-18 17:40:38 -0400

    some more documentation

--- nwk_graph.R
+++ nwk_graph.R
@@ -1,14 +1,6 @@
 # author - Anupama Rajaram
-# attribution - modified code based on blog post. Link below:
-# http://thinktostart.com/analyze-linkedin-with-r/
-
 
 # Add required dev libraries:
-install.packages("devtools")
-# new network package
-devtools::install_github("briatte/ggnet")
-library(ggnet)
-
 library(networkD3)
 library(data.table) #fread() function
 library(network)  # basic 'statnet' network library
@@ -17,41 +9,56 @@ library(sna)
 library(ggplot2)
 library(igraph)
 
+# adding the ggnet (new network package) using devtools:
+install.packages("devtools") 
+devtools::install_github("briatte/ggnet")
+library(ggnet)
 
-# network diag for notre_dam students:
+
+# ============================================================ #
+# part 1 - Simple network diagram using Notre-Dam Univ Data
+# ============================================================ #
+# social network data for univ of notre_dam students:
 ntd <- fread("C:/anu/datasets/other/notre_dame_snw.csv")
 
 # huge dataset, so we only take the first 300 observations.
 validntd <- ntd[1:300,] # select all these rows
-simpleNetwork(validntd)
+
+# plot a simple network! That easy! :)
+simpleNetwork(validntd) 
+# image also uploaded - social_nwk_notre_dame.jpg
 
 
-
-# network diagram for women club.
+# ============================================================ #
+# part 2 - Another network diagram using new ggnet package.
+# ============================================================ #
+# social network data for women club.
 wsnw <- fread("C:/anu/datasets/other/women-social-nwk.csv")
 netw <- network(wsnw, directed = FALSE)
 
 # vertex names
 network.vertex.names(netw) = wsnw$V1
 
-# actaul graph
+# actual graph
 ggnet2(netw, node.size = 6, node.color = "red", edge.size = 1, edge.color = "grey", 
        label = wsnw$V1)
 # note this has duplicate nodes.
 
 
-
-
-
+# since the data is already in an incidence matrix form, 
+# let's try something else! :)
 women <- graph_from_incidence_matrix(wsnw)
 head(women)
 
-nodes2 <- wsnw$V1
-net2.bp <- bipartite.projection(women)
+nodes2 <- wsnw$V1    ## creating labels for the nodes.
 
-simpleNetwork(wsnw)
+net2.bp <- bipartite.projection(women)
+# this command simply means that there are only 2 possibilities , either a connection exists OR not.
+
+simpleNetwork(wsnw)  # diagram example1
 
 plot(net2.bp$proj1, vertex.label.color="black", vertex.label.dist=1,
      vertex.size=7, vertex.label = nodes2)
+     # diagram example2
 
 
